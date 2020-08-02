961b55217412bdc405953408d9ac1188e7a93e06
NEE_TT <Nshmendyuk.ru@gmail.com>
2020-08-02 22:32:47 +0300

    skeleton added

---
+++ src/main/java/ru/sevenseven/cafeteria/CafeteriaApplication.java
@@ -0,0 +1,13 @@
+package ru.sevenseven.cafeteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeteriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeteriaApplication.class, args);
+    }
+
+}
